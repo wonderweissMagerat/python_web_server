9dea03c08d2062fbe1d6f2aea9c2cfa96781782d
zzy <zhaozhenyu_tx@126.com>
2020-12-11 20:36:44 +0800

    js

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/background.jpg') {
+      myImage.setAttribute('src', 'images/background1.jpg');
+    } else {
+      myImage.setAttribute('src', 'images/background.jpg');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('请输入你的名字。');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+  }
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+  }
+
+  myButton.onclick = function() {
+    setUserName();
+ }
